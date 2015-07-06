a71aa711593dab32c823eb4e6972622f53e5080f
Daniel Hall <daniel@danielhall.me>
2015-07-06 15:06:20 +1000

    Initial module

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    # Project Details
+    name='lifx-sdk',
+    version='0.1',
+    packages=['lifx'],
+
+    # Dependencies
+    install_requires=[
+        'bitstruct==1.0.0',
+    ],
+
+    # Metadata for PyPI
+    description='An SDK for local LAN control of bulbs, using Python',
+    author='Daniel Hall',
+    author_email='python-lifx-sdk@danielhall.me',
+    url='http://www.danielhall.me/',
+)
+
