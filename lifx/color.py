7661411f52f020d5b04124981299d5c000561d04
Daniel Hall <daniel@danielhall.me>
2015-08-02 23:46:23 +1000

    Add white

--- lifx/color.py
+++ lifx/color.py
@@ -10,12 +10,15 @@ MID_KELVIN = 5750
 
 HSBK = namedtuple('HSBK', ['hue', 'saturation', 'brightness', 'kelvin'])
 
+# Bright Colors
 RED = HSBK(0, 1, 1, MID_KELVIN)
 YELLOW = HSBK(60, 1, 1, MID_KELVIN)
 GREEN = HSBK(120, 1, 1, MID_KELVIN)
 AQUA = HSBK(180, 1, 1, MID_KELVIN)
 BLUE = HSBK(240, 1, 1, MID_KELVIN)
 PURPLE = HSBK(300, 1, 1, MID_KELVIN)
+WHITE = HSBK(0, 0, 1, MID_KELVIN)
+
 
 def color_from_message(state):
     hue = float(state.hue) / protocol.UINT16_MAX * HUE_MAX
